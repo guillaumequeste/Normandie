8d87c99729407bfa96902ec5fca02e37881f3140
Guillaume Queste <guillaume.queste@laposte.net>
2019-03-28 09:21:19 +0100

    pourville quiberville sainte-marguerite

--- src/app/components/pourville/pourville.component.ts
+++ src/app/components/pourville/pourville.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-pourville',
@@ -10,6 +11,7 @@ export class PourvilleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    AOS.init();
   }
 
 }
